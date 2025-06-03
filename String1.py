e7561539a81d376da14978b2535c1761e900e5ae
aradhanabhoyar <168920602+aradhanabhoyar@users.noreply.github.com>
2025-06-03 18:24:28 +0530

    Add files via upload

---
+++ String1.py
@@ -0,0 +1,42 @@
+s = "instagram"
+print(s[5])
+
+print(s[-2])
+
+print(s[2:6])
+
+print(s[6:9])
+
+print(s[6:len(s)])
+print(len(s))      
+
+print(s[0:len(s)])
+
+print(s[0:9:3])
+
+print(s[ : : 2])
+
+print(s[6: ])
+
+print(s[0:9:+1])
+
+print(s[3:6:-1])
+
+print(s[9:0:-1])
+
+print(s[::-1])
+
+print(s[::-2])
+
+
+## Reverse or Negative 
+
+s = "instagram"
+print(s[-1])
+print(s[-3:])
+print(s[-4:])
+print(s[-9:-4])
+print(s[-7:-3])
+print(s[-5:-2])
+print(s[-9::])     
+print(s[-9:0:-1])         #empty string
